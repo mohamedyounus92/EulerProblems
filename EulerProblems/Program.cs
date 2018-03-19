ab915ad0e47474c5ed8566ce0da9a48fe6cd9726
Mohamed Younus <younus.hakkim@gmail.com>
2018-03-19 23:57:08 +0530

    Adding Sum of Even Fibonacci Numbers

---
+++ EulerProblems/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SumOfEvenFibonacci obj = new SumOfEvenFibonacci();
+            obj.solve();
+        }
+    }
+}
